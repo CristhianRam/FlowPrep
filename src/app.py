71f101a91a3f28c7f2db77ffd853f0ec9bb381a4
CristhianRam <cristhianramc@gmail.com>
2025-07-06 23:12:20 -0600

    First Template Prototype

--- src/app.py
+++ src/app.py
@@ -1,10 +1,51 @@
 from flask import Flask
+from services.preprocessing import *
+from flask import request, render_template, send_file
+import pandas as pd
+import io
+from flask_cors import CORS
 
-app = Flask("FlowPrep")
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return "Hello World"
+@app.route("/")
+def home():
+    return render_template("index.html")  # Renderiza tu HTML desde /templates/
+
+@app.route('/api/process', methods=["POST"])
+def process():
+    print("→ Recibiendo petición /api/process")
+    # Obtener datos del formulario
+    file = request.files.get("file")
+    null_action = request.form.get("null_action")
+
+    if not file:
+        return "No se recibió archivo", 400
+
+    csv_text = file.read().decode("utf-8")
+
+    # Generar DataFrame
+    df = load_data(csv_text)
+
+    # Manejo de nulos
+    if (null_action != "none"):
+        if (null_action == "rm-0"  or null_action == "rm-1"):
+             remove_nulls(df, null_action)
+        else:
+            replace_nulls(df, null_action)
+
+    # Crear CSV en memoria
+    output = io.StringIO()
+    df.to_csv(output, index=False)
+    output.seek(0)
+
+    # Enviar como archivo descargable
+    return send_file(
+        io.BytesIO(output.getvalue().encode()),
+        mimetype='text/csv',
+        as_attachment=True,
+        download_name='processed_data.csv'
+    )
 
 
 if __name__== "__main__":
--- src/services/preprocessing.py
+++ src/services/preprocessing.py
@@ -14,30 +14,33 @@ def load_data(csv_text):
     df = pd.read_csv(csv_buffer)
     return df
 
-def remove_nulls(df, axis):
+def remove_nulls(df, opt):
     """
     Function that remove nulls from the dataframe object.
     axis = 0 to remove rows
     axis = 1 to remove columns
     """
-    df.dropna(axis=axis, inplace=True)
+    if opt == "rm-0": 
+        df.dropna(axis=0, inplace=True)
+    elif opt == "rm-0":
+        df.dropna(axis=1, inplace=True)
 
 def replace_nulls(df, replace_by):
     """Function that replace all the null values in the
         data frame object.
     """
-    if replace_by == "num_mean": # replace nulls with mean in numerical columns
+    if replace_by == "rp-mean": # replace nulls with mean in numerical columns
         df.fillna(df.mean(numeric_only=True), inplace=True)
-    elif replace_by == "num_mode": # replace nulls with mode in numerical columns
+    elif replace_by == "rp-mode": # replace nulls with mode in numerical columns
         df.fillna(df.mode(numeric_only=True), inplace=True) 
-    elif replace_by == "all_mode": # replace nulls with mode in all columns
+    elif replace_by == "rp-mode": # replace nulls with mode in all columns
         df.fillna(df.mode(), inplace=True)
     elif replace_by == "mean_mode": # replace nulls with mean in numerical columns and mode in others
         df.fillna(df.mean(numeric_only=True), inplace=True)
         df.fillna(df.mode(), inplace=True)
-    elif replace_by == "ffill": # fill values by propagating the last valid observation to next valid.
+    elif replace_by == "pr-f": # fill values by propagating the last valid observation.
         df.ffill(inplace=True)
-    elif replace_by == "bfill": # fill values by using the next valid observation to fill the gap.
+    elif replace_by == "pr-b": # fill values by using the next valid observation to fill the gap.
         df.bfill(inplace=True)
         
 
